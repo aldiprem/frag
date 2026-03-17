2d790f71fa3ce92f114f713582fdcecf583ee627
aldiprem <premiumaldi053@gmail.com>
2026-03-18 04:12:37 +0700

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -91,4 +91,4 @@ async def purchase():
         return jsonify({'success': False, 'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5501, debug=True)
+    app.run(host='127.0.0.1', port=5500, debug=True)
